3b3e01420c286402fb72acdf811d685a7b1355fa
jharris10 <jharris@paloaltonetworks.com>
2019-12-18 13:35:52 +0000

    initial

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -1,17 +1,15 @@
 output "INBOUND-MGMT-FW1" {
-  value = "https://${module.vmseries-in.nic0_public_ip[0]}"
+  value = "https://${module.vmseries-in.nic1_private_ip[0]}"
 }
 
-
-
 output "INBOUND-MGMT-FW2" {
-  value = "https://${module.vmseries-in.nic0_public_ip[1]}"
+  value = "https://${module.vmseries-in.nic1_private_ip[1]}"
 }
 
 output "OUTBOUND-MGMT-FW1" {
-  value = "https://${module.vmseries-out.nic0_public_ip[0]}"
+  value = "https://${module.vmseries-out.nic1_private_ip[0]}"
 }
 
 output "OUTBOUND-MGMT-FW2" {
-  value = "https://${module.vmseries-out.nic0_public_ip[1]}"
+  value = "https://${module.vmseries-out.nic1_private_ip[1]}"
 }
